726ff568a853cf9cd53653e2cc6aa7b3fa311b99
Tal Ben Adon <talbenadon2@gmail.com>
2024-09-18 16:21:50 +0300

    intiated sass, added Noto Sans fonts and initiated router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Root from './routes/root';
+import './index.scss'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
+

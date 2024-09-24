ac58dd5826eb27b486bfc8910465c1a2ddf716d3
Tal Ben Adon <talbenadon2@gmail.com>
2024-09-24 04:25:37 +0300

    spacing

--- src/errorBoundary/index.tsx
+++ src/errorBoundary/index.tsx
@@ -8,10 +8,8 @@ interface ErrorBoundaryState {
     hasError: boolean;
 }
 
-
 class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
     state: ErrorBoundaryState = { hasError: false };
-
 
     static getDerivedStateFromError(_error: Error): Partial<ErrorBoundaryState> {
         return { hasError: true }
@@ -20,14 +18,13 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
     componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
         // TODO: logErrorToService(error, errorInfo)
     }
+
     render(): ReactNode {
         if (this.state.hasError) {
             return <h1>Something went wrong</h1>
         }
-
         return this.props.children
     }
 }
-
 
 export default ErrorBoundary
